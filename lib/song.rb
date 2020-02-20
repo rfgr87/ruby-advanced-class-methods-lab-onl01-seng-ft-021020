6ae35dd29b77a27f950f1f6e6a086e9840d25472
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-20 20:08:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,12 @@ class Song
     self.class.all << self
   end
   
-  def self.create(name)
+  def self.create
     song = self.new
-    song.name = name
-    @@all << 
+    @@all << song
+  end
+  
+  def 
     
         
 
