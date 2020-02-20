c1263bc16c4ec46ee144ba330587d3995f22673c
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-20 21:05:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
   def self.find_or_create_by_name(name)
     song = self.find_by_name(name)
     if song == false
-      song = self.create_by_name(name)
+      return self.create_by_name(name)
     else
       song
     end
