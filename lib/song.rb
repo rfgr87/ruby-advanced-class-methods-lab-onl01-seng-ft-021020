221d63c8064e11bac0da5291ddcad980a7bc774a
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-20 20:22:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,45 @@ class Song
     @@all << song
   end
   
-  def 
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+  end
+  
+  def self.find_by_name(name)
+    song = @@all.collect do |x|
+      if x.name == name
+        x 
+      end
+      song
+    end 
+  end
+  
+  def self.find_or_create_by_name(name)
+    song = @@all.collect do |x|
+      if x.name == name
+        x 
+      else
+        nil
+      end
+    end
+    if song == nil
+      song = self.new
+      song.name = name
+      song
+    end
+  end
+  
+  def self.alphabetical
+    
+  
     
         
 
